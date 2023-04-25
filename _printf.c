99b041baabaaf8d0fe68351829551ca71aac4ac9
bbumblebe <107593421+bbumblebe@users.noreply.github.com>
2023-04-25 20:27:02 +0200

    Try 2

--- _printf.c
+++ _printf.c
@@ -6,6 +6,27 @@
 int print_number(int n, int base, int is_signed);
 int print_hex(unsigned int n, int uppercase);
 int print_pointer1(void *p);
+int _putchar(char c);
+
+/**
+
+ * _putchar - writes char c to stdout
+
+ * @c: the char to print
+
+ * Return: on success 1
+
+ * on error, -1 is retirned
+
+ 
+
+int _putchar(char c)
+
+{
+
+	return (write(1, &c, 1));
+*/
+
 
 
 int len;
@@ -308,4 +329,22 @@ int print_pointer1(void *ptr)
 
     return (count);
 
+}
+/**
+
+ * _putchar - writes char c to stdout
+
+ * @c: the char to print
+
+ * Return: on success 1
+
+ * on error, -1 is retirned
+
+ */
+
+int _putchar(char c)
+
+{
+
+	return (write(1, &c, 1));
 }
