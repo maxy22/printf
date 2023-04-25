718de26c216cecd0e91ecc0fdcd439ae4b401776
bbumblebe <107593421+bbumblebe@users.noreply.github.com>
2023-04-25 15:34:07 +0200

    Main.h files

--- main.h
+++ main.h
@@ -1,6 +1,30 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
 #include <stdarg.h>
+
 #include <stdio.h>
+
+#include <stdlib.h>
+
 #include <unistd.h>
-#endif
+
+#include <limits.h>
+
+int _printf(const char *format, ...);
+
+int print_number(int n, int base, int signed_);
+
+int print_hex(unsigned int n, int uppercase);
+
+int print_pointer(void *ptr);
+
+#endif /* MAIN_H */ 
+
+
+
+
+
+
+
