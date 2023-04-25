6f3be5b31ce5d8fa811e7f969e16c69f86f62b3e
bbumblebe <107593421+bbumblebe@users.noreply.github.com>
2023-04-25 23:48:20 +0200

    Aaa

--- _printf.c
+++ _printf.c
@@ -40,12 +40,12 @@ int _printf(const char *format, ...)
 
     int printed = 0, i = 0;
 
-     /**c = '\0', type = '\0';
+   /* c = '\0', type = '\0';
 
-    unsigned int u = 0;
-    if (char *s = NULL, *out = NULL, c = '\0', type = '\0')
+    unsigned int u = 0;*/
+    char *s = NULL, *out = NULL, c = '\0', type = '\0';
 
-    void *ptr = NULL;*/
+    void *ptr = NULL;
 
     if (!format || (format[0] == '%' && format[1] == '\0'))
 
