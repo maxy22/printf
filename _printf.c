24ca27004b585f3e75562892521b820b39aa6c89
maxy22 <mdotsey17@gmail.com>
2023-04-26 06:37:10 -0700

    printf update

--- _printf.c
+++ _printf.c
@@ -1,467 +1,67 @@
-#include <stdarg.h>
+#include "main.h"
 
-#include <string.h>
-
-#include <stdio.h>
-
-int print_number(int n, int base, int is_signed);
-
-int print_hex(unsigned int n, int uppercase);
-
-int print_pointer1(void *p);
-
-void print_formatted_string(const char* format, ...);
-
-
-
-int len;
-
-int i;
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
-
- * _printf - A custom implementation of printf
-
- * @format: A string containing zero or more directives to be printed
-
- *
-
- * Return: The numer of characters printed (excluding the null byte used to end
-
- * the output to strings), or a negative value if an error occurs.
-
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
-
 {
-
-    va_list args;
-
-    int printed = 0, i = 0;
-
-   /* c = '\0', type = '\0';
-
-    unsigned int u = 0;*/
-    char c = '\0', type = '\0';
-
-    void *ptr = NULL;
-
-    if (!format || (format[0] == '%' && format[1] == '\0'))
-
-        return (-1);
-
-    va_start(args, format);
-
-    for (i = 0; format[i]; i++)
-
-    {
-
-        if (format[i] == '%')
-
-        {
-
-            i++;
-
-            if (format[i] == '\0')
-
-                return (-1);
-
-            type = format[i];
-
-            switch (type)
-
-            {
-
-              /**  case 'c':
-
-                    c = (char) va_arg(args, int);
-
-                    out = &c;
-
-                    len = 1;
-
-                    break;*/
-		    
-			
-               case 'c': 
-
-                     c = (char) va_arg(args, int);
-
-                    putchar(c);
-
-                    len = 1;
-
-                    break;
-	
-
-	
-
-	
-
-	
-
-               /** case 's':
-
-                    s = va_arg(args, char *);
-
-                    if (s == NULL)
-
-                        s = "(null)";
-
-                    out = s;
-
-                    len = strlen(out);
-
-                    break;*/
-			    case 's':
-
-    {
-
-        char* s = va_arg(args, char*);
-
-        if (s == NULL) {
-
-            s = "(null)";
-
-        }
-
-        fputs(s, stdout);
-
-        len = strlen(s);
-
-        break;
-
-    }
-			    /**case 's':
-
-                     char* s;
-			    s = va_arg(args, const char*);
-			
-
-                    if (s == NULL) {
-
-                        s = "(null)";
-
-                    }
-
-                    fputs(s, stdout);
-
-                    len = 1;
-
-                    break;*/
-			
-
-                case 'd':
-
-                case 'i':
-
-                    printed += print_number(va_arg(args, int), 10, 1);
-
-                    break;
-
-                case 'u':
-
-                    printed += print_number(va_arg(args, unsigned int), 10, 0);
-
-                    break;
-
-                case 'o':
-
-                    printed += print_number(va_arg(args, unsigned int), 8, 0);
-
-                    break;
-
-                case 'x':
-
-                case 'X':
-
-                    printed += print_hex(va_arg(args, unsigned int), (type == 'X'));
-
-                    break;
-
-                case 'p':
-
-                    ptr = va_arg(args, void *);
-
-                    printed += print_pointer1(ptr);
-
-                    break;
-
-                case '%':
-
-                    putchar('%');
-
-                    printed++;
-
-                    break;
-
-                case '\0':
-
-                    return (-1);
-
-                default:
-
-                    putchar('%');
-
-                    putchar(type);
-
-                    printed += 2;
-
-                    break;
-
-            }
-
-        }
-
-        else
-
-        {
-
-            putchar(format[i]);
-
-            printed++;
-
-        }
-
-    }
-
-    va_end(args);
-
-    return (printed);
-
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
 /**
-
- * print_number - Prints an integer to stdout
-
- * @n: The integer to print
-
- * @base: The base to use when printing the integer
-
- * @signed_: Whether or not the integer is signed
-
- *
-
- * Return: The number of characters printed
-
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-
-int print_number(int n, int base, int signed_)
-
+void print_buffer(char buffer[], int *buff_ind)
 {
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-    int count = 0;
-
-    unsigned int num = (signed_ && n < 0) ? -n : n;
-
-    char digits[] = "0123456789abcdef";
-
-    if (signed_ && n < 0)
-
-        putchar('-'), count++;
-
-    if (num / base)
-
-        count += print_number(num / base, base, 0);
-
-    putchar(digits[num % base]);
-
-    count++;
-
-    return (count);
-
+	*buff_ind = 0;
 }
 
-/**
-
- * print_hex - Prints a number in hexadecimal form to stdout
-
- * @n: The number to print
-
- * @uppercase: Whether or not to print in uppercase
-
- *
-
- * Return: The number of characters printed
-
- */
-
-int print_hex(unsigned int n, int uppercase)
-
-{
-
-    int count = 0;
-
-    char *digits = "0123456789abcdef";
-
-    if (uppercase)
-
-    digits = "0123456789ABCDEF";
-
-    if (n / 16)
-
-        count += print_hex(n / 16, uppercase);
-
-    putchar(digits[n % 16]);
-
-    count++;
-
-    return (count);
-
-}
-
-/**
-
- * print_pointer - Prints a pointer address in hexadecimal form to stdout
-
- * @ptr: The pointer to print
-
- *
-
- * Return: The number of characters printed
-
- */
-
-int print_pointer(void *ptr)
-
-{
-
-    int count = 0;
-
-    unsigned long int n = (unsigned long int) ptr;
-
-    char digits[] = "0123456789abcdef";
-
-    count += printf("0x");
-
-    if (n / 16)
-
-        count += print_hex(n / 16, 0);
-
-    putchar(digits[n % 16]);
-
-    count++;
-
-    return (count);
-
-}
-
-/**
-
- * print_pointer - Prints a pointer address to stdout
-
- * @ptr: The pointer to print
-
- *
-
- * Return: The number of characters printed
-
- */
-
-int print_pointer1(void *ptr)
-
-{
-
-    unsigned long int address = (unsigned long int) ptr;
-
-    int count = 0;
-
-    count += printf("0x%lx", address);
-
-    return (count);
-
-}
-
-
-/**
-
- * _putchar - writes char c to stdout
-
- * @c: the char to print
-
- * Return: on success 1
-
- * on error, -1 is retirned
-
-
-
-int putchar(char c)
-
-{
-
-	return (write(1, &c, 1));
-}
-*/
-void print_formatted_string(const char* format, ...) {
-
-	
-    const char* p = format;
-	va_list args;
-
-    va_start(args, format);
-
-    
-
-    while (*p != '\0') {
-
-        if (*p == '%') {
-
-            switch (*(p + 1)) {
-
-                case 'c': {
-
-                    char c = (char) va_arg(args, int);
-
-                    putchar(c);
-
-                    p += 2;
-
-                    break;
-
-                }
-
-                case 's': {
-
-                    const char* s = va_arg(args, const char*);
-
-                    if (s == NULL) {
-
-                        s = "(null)";
-
-                    }
-
-                    fputs(s, stdout);
-
-                    p += 2;
-
-                    break;
-
-                }
-
-                default: {
-
-                    putchar(*p);
-
-                    p += 1;
-
-                    break;
-
-                }
-
-            }
-
-        } else {
-
-            putchar(*p);
-
-            p += 1;
-
-        }
-
-    }
-
-    va_end(args);
-
-}
